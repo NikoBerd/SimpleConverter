79902a89b2733dd7ea8ec6a6d85143d9e151acfa
Niko Berdzenishvili <nikolozisia@gmail.com>
2023-03-23 22:58:19 +0400

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Niko on 23.03.23.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
